47a8a3be37134cd0c2c92bd53f231edb01fc798e
Patricio Leonel Filice <patricio.filice@gmail.com>
2022-11-05 20:53:00 -0300

    Changes - Fixed agents initialization without state

--- lib/app/domain/to_do_list_agent.ex
+++ lib/app/domain/to_do_list_agent.ex
@@ -5,8 +5,16 @@ defmodule App.ToDoList.Agent do
 
   def start_link(init_arg) do
     Agent.start_link(fn ->
+      reducer = fn { agent_pid, _ }, map ->
+        if (map == %{}) do
+          App.ToDoList.Agent.get(agent_pid)
+        else
+          map
+        end
+      end
+      map = Enum.reduce(Registry.lookup(App.ToDoList.Agent.Registry, App.ToDoList.Agent), %{}, reducer)
       Registry.register(@to_do_list_agent_registry, App.ToDoList.Agent, init_arg)
-      %{}
+      map
     end)
   end
 
@@ -15,6 +23,10 @@ defmodule App.ToDoList.Agent do
       id: init_arg,
       start: { __MODULE__, :start_link, [init_arg] }
     }
+  end
+
+  def get(agent_pid) do
+    Agent.get(agent_pid, & &1)
   end
 
   def get(agent_pid, key) do
